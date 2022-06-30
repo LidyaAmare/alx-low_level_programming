0c7f7fa6e5ea01da678c7840e3db9bccc59cd073
LidyaAmare <lidyaamare76@gmail.com>
2022-06-30 07:59:35 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,6 @@
 */
 int main(void)
 {
-writef(2,"and that piece of art is useful\"- Dora Korpar,2015-10-19,\n");
-
+writef("and that piece of art is useful - Dora Korpar,2015-10-19,\n");
 return (1);
 }

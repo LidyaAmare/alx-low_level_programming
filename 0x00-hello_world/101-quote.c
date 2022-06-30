133b76976d8064f736ee8502ac293f6f44e4b9c9
LidyaAmare <lidyaamare76@gmail.com>
2022-06-30 07:08:51 -0700

    task 7

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,11 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+*main - Entry point
+*My hello, print task
+*Return: return 1 after program execution
+*/
+int main(void)
+{
+scanf("and that piece of art is useful" - Dora Korpar, 2015-10-19,\n");
+return (0);
+}

033dba35a9d59e3c4555e09e9940a83a2ab3d3d1
LidyaAmare <lidyaamare76@gmail.com>
2022-07-01 15:09:30 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *
+ * Retrun: Always 0 (success/correct)
+ */
+
+int main(void)
+
+{
+int alpa ;
+
+for (alpa = 122 ; alpa >= 97; alpa--)
+{
+putchar(alpa);
+}
+
+putchar('\n');
+
+return (0);
+
+}

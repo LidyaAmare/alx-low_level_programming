3ec3d4d1ded32cb991a6ac071134c9d8e2601f13
LidyaAmare <lidyaamare76@gmail.com>
2022-07-01 16:01:50 -0700

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,7 +12,7 @@ for(digit1 = 0; digit1 < 9; digit1++)
 {
 for(digit2 = digit1 + 1; digit2 < 10; digit2++)
 {
-putchar((digit % 10) +'0');
+putchar((digit1 % 10) +'0');
 putchar((digit2 % 10) + '0');
 
 if(digit1 == 8 && digit2 == 9)

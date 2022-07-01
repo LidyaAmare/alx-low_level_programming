e44123bc3653ce8fe3ce8bfcfc4ad0035a67c087
LidyaAmare <lidyaamare76@gmail.com>
2022-07-01 13:43:12 -0700

    task 6

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -15,7 +15,7 @@ for(lower_case = 'a'; lower_case <= 'z'; lower_case++)
 putchar(lower_case);
 }
 
-for(upper_case = 'A'; upper_case <= 'Z'; lowe_case++)
+for(upper_case = 'A'; upper_case <= 'Z'; lower_case++)
 {
 putchar(upper_case);
 }

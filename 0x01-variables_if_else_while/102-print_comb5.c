e6898aea9b8dd0e5d4d81ed122188b2a1eb874a2
LidyaAmare <lidyaamare76@gmail.com>
2022-07-01 18:31:46 -0700

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,29 +16,29 @@ int i, j;
 
 			for (j = 0; j < 100; j++)
 
-			{
+{
 
-				if (i < j)
+if (i < j)
 
-				{
+{
 
-					putchar((i / 10) + 48);
+putchar((i / 10) + 48);
 
-						putchar((i % 10) + 48);
+  putchar((i % 10) + 48);
 
-							putchar(' ');
+     putchar(' ');
 
-								putchar((j / 10) + 48);
+		putchar((j / 10) + 48);
 
-									putchar((j % 10) + 48);
+						putchar((j % 10) + 48);
 
-				if (i != 98 || j != 99)
+if (i != 98 || j != 99)
 
-				{
+{
 
-					putchar(',');
+putchar(',');
 
-						putchar(' ');
+  putchar(' ');
 
 
 				}

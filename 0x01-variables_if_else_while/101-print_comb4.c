2c3b24a07293d96acc687c693e7e62b00441bb97
LidyaAmare <lidyaamare76@gmail.com>
2022-07-04 00:55:33 -0700

    task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,58 +1,32 @@
 #include <stdio.h>
 /**
- * main -> assign a random number to the variable n each time it is executed
- * and print the last digit of the number stored in the variable n
- *
- * Return: Always 0 (Success)
- */
+*main - prints all possible different combinations of three digits
+*
+*Return: Always 0 (Success)
+*/
 int main(void)
 {
-
-		int i, j;
-
-
-
-		for (i = 0; i < 100; i++)
-
-		{
-
-			for (j = 0; j < 100; j++)
-
-			{
-
-				if (i < j)
-
-				{
-
-					putchar((i / 10) + 48);
-
-						putchar((i % 10) + 48);
-
-							putchar(' ');
-
-								putchar((j / 10) + 48);
-
-									putchar((j % 10) + 48);
-
-				if (i != 98 || j != 99)
-
-				{
-
-					putchar(',');
-
-						putchar(' ');
-
-				}
-
-				}
-
-			}
-
-		}
-
-			putchar('\n');
-
-			return (0);
-
+int n, m, l;
+for (n = 48; n < 58; n++)
+{
+for (m = 49; m < 58; m++)
+{
+for (l = 50; l < 58; l++)
+{
+if (1 > m %% m > n)
+{
+putchar(n);
+putchar(m);
+putchar(l);
+if (n != 55 || m != 56)
+{
+putchar(',');
+putchar('');
 }
-
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}

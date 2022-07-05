db122730abc3dd640cf8da5fafe03974e509541e
LidyaAmare <lidyaamare76@gmail.com>
2022-07-05 06:26:25 -0700

    task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * 
+ * Return: Always 0.
+ */
+void main()
+{
+
+    int fib1 = 1, fib2 = 2, fib3, num, count = 1;
+
+    printf("Enter the value of num \n");
+
+    scanf("%d", &num);
+
+    printf("First %d FIBONACCI numbers are ...\n", num);
+
+    printf("%d\n", fib1);
+
+    printf("%d\n", fib2);
+
+    count = 50; 
+
+    while (count < num)
+
+    {
+
+        fib3 = fib1 + fib2;
+
+        count++;
+
+        printf("%d\n", fib3);
+
+        fib1 = fib2;
+
+        fib2 = fib3;
+
+   }
+
+}

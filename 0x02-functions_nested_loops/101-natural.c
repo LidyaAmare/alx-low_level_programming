4bd11fdf49c1b97a418b80550a899c9e5f8f93a6
LidyaAmare <lidyaamare76@gmail.com>
2022-07-05 05:57:02 -0700

    task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - natural numbers
+*
+*Return: Always 0.
+*/
+int main(void)
+{
+int a, j;
+for (a = 1; a < 1024; a++)
+{
+if ((a % 3) == 0 || (a % 5) == 0)
+j +=a;
+}
+printf("%d\n", j);
+return (0);
+}

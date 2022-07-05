f7a28b26a0a6dd755bd9b1b0367a15c41f9d0380
LidyaAmare <lidyaamare76@gmail.com>
2022-07-05 07:15:17 -0700

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,5 +6,19 @@
 */
 void print_times_table(int n)
 {
+print_times_table(3);
+
+_putchar('\n');
+
+print_times_table(5);
+
+_putchar('\n');
+
+print_times_table(98);
+
+_putchar('\n');
+
+print_times_table(12);
+
 return (0);
 }

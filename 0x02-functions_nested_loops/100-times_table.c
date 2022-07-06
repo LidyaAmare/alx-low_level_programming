2fed0e8dfa0b6d2a87c722c21930b30bec3074b9
LidyaAmare <lidyaamare76@gmail.com>
2022-07-06 01:14:50 -0700

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -25,14 +25,16 @@ _putchar(' ');
 _putchar(' ');
 _putchar(' ');
 _putchar(k + '0');
-} else if (k >= 10 && k < 100)
+}
+else if (k >= 10 && k < 100)
 {
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar((k / 10) + '0');
 _putchar((k % 10) + '0');
-} else if (k >= 100)
+}
+else if (k >= 100)
 {
 _putchar(',');
 _putchar(' ');

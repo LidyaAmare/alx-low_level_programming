19584f731f6841e49d17c19d512dbd5af075963b
LidyaAmare <lidyaamare76@gmail.com>
2022-07-07 04:02:41 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,19 +6,12 @@
  */
 void more_numbers(void)
 {
-
 int i;
-
 int first_number;
-
 int second_number;
-
 int result;
-
 i = 0;
-
 result = 0;
-
 while (i < 10)
 {
 
@@ -27,31 +20,20 @@ while (result <= 14)
 
 if (result < 10)
 {
-
 second_number = result;
 }
 
 else
 {
-
 first_number = result / 10;
-
 second_number = result % 10;
-
 _putchar (first_number + '0');
-
 }
-
 _putchar (second_number + '0');
-
 result++;
-
 }
-
 i++;
-
 result = 0;
-
 _putchar ('\n');
 
 }

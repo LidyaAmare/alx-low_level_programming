abc7243f640ef1b5cccd97625417fb1eb9f36941
LidyaAmare <lidyaamare76@gmail.com>
2022-07-07 04:06:42 -0700

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,56 +6,34 @@
  */
 int main(void)
 {
-
 int p = 100;
 int i;
-
 i = 1;
-
 while (i <= p)
 {
 
 if (i % 3 == 0 && i % 5 == 0)
 {
-
 printf("FizzBuzz ");
-
 }
 
 else if (i % 3 == 0)
 {
-
 printf("Fizz ");
-
 }
-
 else if (i % 5 == 0)
-
 {
-
 if (i < p)
-
 printf("Buzz ");
-
 else
-
 printf("Buzz");
-
 }
-
 else
 {
-
 printf("%i ", i);
-
 }
-
 i++;
-
 }
-
 printf("\n");
-
 return (0);
-
 }

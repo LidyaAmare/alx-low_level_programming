bae472be9ad9a564445b1489aa0b79d43bb13f32
LidyaAmare <lidyaamare76@gmail.com>
2022-07-29 07:00:08 -0700

    task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -9,11 +9,9 @@ int main(int argc, char *argv[])
 {
 unsigned long mul;
 int i, j;
-
 if (argc != 3)
 
 { printf("Error\n");
-
 
 exit(98); }
 
@@ -32,11 +30,9 @@ if (argv[i][j] > 57 || argv[i][j] < 48)
 exit(98); }
 
 }
-
 }
 
-mul = atol(argv[1]) * atol(argv[2]);
-
+mul = atol(argv[1]) *  atol(argv[2]);
 printf("%lu\n", mul);
 
 return (0);

6b29d5e409d195d761c91cea342d276559ffbc5e
LidyaAmare <lidyaamare76@gmail.com>
2022-08-01 02:19:26 -0700

    task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file program was compiled from
+ * Return: Always 0
+ */
+
+int main(void)
+
+{
+
+printf("%s\n", __FILE__);
+
+return (0);
+
+}

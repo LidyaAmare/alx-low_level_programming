6bdf441b8f5db0da0ef8d74d3da265911792275f
LidyaAmare <lidyaamare76@gmail.com>
2022-08-10 22:34:04 -0700

    task 4

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * bbmain - function executted before main
+ *
+ * Return: void
+ */
+
+void __attribute__ ((constructor)) bbmain()
+
+{
+
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+
+}

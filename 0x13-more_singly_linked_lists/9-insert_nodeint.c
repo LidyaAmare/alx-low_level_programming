2d3d680ecb804712af48a6176d08059a5c642a94
LidyaAmare <lidyaamare76@gmail.com>
2022-08-16 05:06:28 -0700

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,7 +8,6 @@
  *
  * Return: pointer to the new node, or NULL
  */
-
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 {
@@ -22,19 +21,19 @@ return (NULL);
 new->n = n;
 new->next = NULL;
 if (idx == 0)
-
 {
 
 new->next = *head;
-ead = new;
+*head = new;
 return (new);
 
 }
 
 for (i = 0; temp && i < idx; i++)
-
 {
+
 if (i == idx - 1)
+
 {
 
 new->next = temp->next;
